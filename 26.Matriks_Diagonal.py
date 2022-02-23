8cee3c680536598be04132fa8fc9d6bf8b6fac81
Imam Muhajir <39195682+muhajir29@users.noreply.github.com>
2022-02-23 18:39:33 +0700

    Update 26.Matriks_Diagonal.py

--- 26.Matriks_Diagonal.py
+++ 26.Matriks_Diagonal.py
@@ -1,3 +1,44 @@
+'''
+ Create 26.Matriks_Diagonal.py
+
+Buatlah program yang dapat menghasilkan matriks persegi diagonal berukuran n x n.
+Diagonal dari matriks diisi dengan angka-angka yang diberikan sebagai input,
+sedangkan sisanya diisi nol.
+
+Format Masukan
+Masukan terdiri atas dua baris. Baris pertama adalah n, yaitu ukuran baris dan kolom matriks (1 <= n <= 100).
+Baris kedua adalah angka-angka sebanyak n, dengan masing-masing angka dalam rentang 0 <= x <= 9.
+
+Format Keluaran
+Keluaran adalah matriks berukuran n x n sesuai ketentuan. Setiap elemen matriks dipisahkan dengan satu spasi, 
+kecuali kolom terakhir yang langsung berupa newline tanpa spasi.
+
+Contoh Masukan 1
+1
+7
+
+Contoh Keluaran 1
+7
+
+Contoh Masukan 2
+10
+0 7 6 2 5 0 3 5 9 7
+
+Contoh Keluaran 2
+0 0 0 0 0 0 0 0 0 0
+0 7 0 0 0 0 0 0 0 0
+0 0 6 0 0 0 0 0 0 0
+0 0 0 2 0 0 0 0 0 0
+0 0 0 0 5 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 3 0 0 0
+0 0 0 0 0 0 0 5 0 0
+0 0 0 0 0 0 0 0 9 0
+0 0 0 0 0 0 0 0 0 7
+
+'''
+
+
 x =  int(input())
 masuk = list(map(int, input().split()))
  
